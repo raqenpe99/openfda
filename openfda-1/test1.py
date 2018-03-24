8be2488b88510aa2c554a2595642e9437d0241bc
raqenpe99 <37545392+raqenpe99@users.noreply.github.com>
2018-03-24 13:59:12 +0100

    Delete test1.py

--- openfda-1/test1.py
+++
@@ -1,8 +0,0 @@
-import json, urllib.request
-with urllib.request.urlopen("https://api.fda.gov/drug/label.json") as web:
-    info = json.loads(web.read().decode())
-
-
-print("El identificador del medicamento es", info.get("results")[0]["id"])
-print("El propósito del medicamente es", str(info.get("results")[0]["purpose"]).strip("[]").strip("'"))
-print("El nombre del fabricante es", str(info.get("results")[0]["openfda"]["manufacturer_name"][0]))
