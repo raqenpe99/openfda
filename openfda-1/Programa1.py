ed4f54034539a798e02f93c8b1701b7308d03865
raqenpe99 <37545392+raqenpe99@users.noreply.github.com>
2018-04-03 21:13:16 +0200

    Update Programa1.py

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -1,8 +1,13 @@
-import json, urllib.request
-with urllib.request.urlopen("https://api.fda.gov/drug/label.json") as web:
-    info = json.loads(web.read().decode())
+import json
+import urllib.request #Request envía cabeceras HTTP a la URL de destino
+                      #Open permite que sean leídas posteriormente
 
+with urllib.request.urlopen("https://api.fda.gov/drug/label.json") as API: #Loads permite tomar la cadena de texto en formato JSON 
+    info = json.loads(API.read().decode())                                 #y convertirla en una estructura tipo diccionario, al
+                                                                           #leer y descondificar la información de la url
+        
+#Encontramos los resultados del medicamento por defecto y, en caso de que no exista esa llave, devuelve el valor por defecto "None":
 
-print("El identificador del medicamento es", info.get("results")[0]["id"])
-print("El propósito del medicamente es", str(info.get("results")[0]["purpose"]).strip("[]").strip("'"))
-print("El nombre del fabricante es", str(info.get("results")[0]["openfda"]["manufacturer_name"][0]))
+print("Identificador del medicamento:", info.get("results")[0]["id"])
+print("Propósito:", info.get("results")[0]["purpose"][0])
+print("Fabricante:", info.get("results")[0]["openfda"]["manufacturer_name"][0])
