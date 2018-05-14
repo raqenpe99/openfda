81b7c288eb8737dfd2bf99074a1be26725c88687
Raquel <37545392+raqenpe99@users.noreply.github.com>
2018-05-14 14:01:38 +0200

    Modificación de extensiones

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -6,235 +6,311 @@ import json
 PORT = 8000
 socketserver.TCPServer.allow_reuse_address = True
 
-
 class TestHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def get_conection(self, limit=1, busqueda="", name=""):
-
+        
         headers = {'User-Agent': 'http-client'}
-        peticion = "/drug/label.json?limit={}".format(limit)
+        peticion= "/drug/label.json?limit={}".format(limit)
 
         if busqueda != "":
-            peticion += '&search=' + busqueda + ':' + name
+            peticion += '&search='+busqueda+':'+ name
 
         print("Recurso solicitado: {}".format(peticion))
-
+        
         conexion = http.client.HTTPSConnection("api.fda.gov")
-        conexion.request("GET", peticion, None, headers)
+        conexion.request("GET", peticion , None, headers)
+
 
         JSON = conexion.getresponse()
+        
+        if JSON.status == 404:
+            print("ERROR. Recurso no encontrado")
+            exit(1)
+            
         print(JSON.status, JSON.reason)
         info = json.loads(JSON.read().decode("utf-8"))
-
+        
         conexion.close()
-
+        
         return info
-
+    
     def get_listDrugs(self, limit):
-
-        info = self.get_conection(limit)
-        drug = info["results"]
+        
+        info=self.get_conection(limit)
+        drug=info["results"] 
         contenido = (' <!DOCTYPE html>\n'
-                     '<html lang="es">\n'
-                     '<head>\n'
-                     '    <meta charset="UTF-8">\n'
-                     '</head>\n'
-                     '<body>\n'
-                     '<p>Nombre.  Fabricante. ID. Propósito</p>'
-                     '\n'
-                     '<ul>\n')
+           '<html lang="es">\n'
+           '<head>\n'
+           '    <meta charset="UTF-8">\n'
+           '</head>\n'
+           '<body>\n'
+           '<p>Nombre.  Fabricante. ID. Propósito</p>'
+           '\n'
+           '<ul>\n')
 
         for i in range(len(drug)):
-            id = drug[i]["id"]
-            purpose = drug[i].get("purpose", ["No aparece información al respecto"])[0]
+            id= drug[i]["id"]
+            purpose=drug[i].get("purpose", ["No aparece información al respecto"])[0]
 
             if drug[i]["openfda"]:
-                name = drug[i]["openfda"]["generic_name"][0]
-                manufacturer = drug[i]["openfda"]["manufacturer_name"][0]
-
-            contenido += '<p>' + name + manufacturer + id + purpose + '</p>'
-
+                name=drug[i]["openfda"]["generic_name"][0]
+                manufacturer= drug[i]["openfda"]["manufacturer_name"][0]
+                
+            contenido+= '<p>'+name+manufacturer+id+purpose+'</p>'
+            
         return contenido
-
+    
     def get_listCompanies(self, limit):
-
-        info = self.get_conection(limit)
-        drug = info["results"]
-
+        
+        info=self.get_conection(limit)
+        drug=info["results"]
+        
         contenido = (' <!DOCTYPE html>\n'
-                     '<html lang="es">\n'
-                     '<head>\n'
-                     '    <meta charset="UTF-8">\n'
-                     '</head>\n'
-                     '<body>\n'
-                     '<p>Fabricantes</p>'
-                     '\n'
-                     '<ul>\n')
+               '<html lang="es">\n'
+               '<head>\n'
+               '    <meta charset="UTF-8">\n'
+               '</head>\n'
+               '<body>\n'
+               '<p>Fabricantes</p>'
+               '\n'
+               '<ul>\n')
 
         for i in range(len(drug)):
             if drug[i]["openfda"]:
-                manufacturer = drug[i]["openfda"]["manufacturer_name"][0]
+                manufacturer=drug[i]["openfda"]["manufacturer_name"][0]
             else:
-                manufacturer = "Desconocido"
-
-            contenido += '<p>' + manufacturer + '</p>'
-
+                manufacturer="Desconocido"
+                
+            contenido+= '<p>'+manufacturer+'</p>'
+        
+        return contenido
+    
+    def get_listWarnings(self, limit):
+        
+        info=self.get_conection(limit)
+        drug=info['results']
+        
+        contenido = (' <!DOCTYPE html>\n'
+               '<html lang="es">\n'
+               '<head>\n'
+               '    <meta charset="UTF-8">\n'
+               '</head>\n'
+               '<body>\n'
+               '<p>Precauciones</p>'
+               '\n'
+               '<ul>\n')
+        
+        for i in range(len(drug)):
+                 
+            if "warnings" in drug[i].keys():
+                warning=drug[i]['warnings'][0]
+            else:
+                warning = "Desconocido"
+            
+            contenido+= '<p>'+warning+'</p>'
+            
         return contenido
 
+    
     def get_searchDrug(self, limit, busqueda, name):
-
-        info = self.get_conection(limit, busqueda, name)
-        drug = info["results"]
-
+        
+        info=self.get_conection(limit, busqueda, name)
+        drug=info["results"]
+        
         contenido = (' <!DOCTYPE html>\n'
-                     '<html lang="es">\n'
-                     '<head>\n'
-                     '    <meta charset="UTF-8">\n'
-                     '</head>\n'
-                     '<body>\n'
-                     '<p>Identifiadores de los farmacos con ese principio activo</p>'
-                     '\n'
-                     '<ul>\n')
-
+               '<html lang="es">\n'
+               '<head>\n'
+               '    <meta charset="UTF-8">\n'
+               '</head>\n'
+               '<body>\n'
+               '<p>Identifiadores de los farmacos con ese principio activo</p>'
+               '\n'
+               '<ul>\n')
+        
         for i in range(len(drug)):
-            id = drug[i]["id"]
-
-            contenido += '<p>' + id + '</p>'
-
+            id= drug[i]["id"]
+                
+            contenido+= '<p>'+id+'</p>'
+        
         return contenido
-
-    def get_searchCompany(self, limit, busqueda, name):
-
-        info = self.get_conection(limit, busqueda, name)
-        drug = info["results"]
-
+    
+    def get_searchCompany(self,limit, busqueda, name):
+    
+        info=self.get_conection(limit, busqueda, name)
+        drug=info["results"]
+        
         contenido = (' <!DOCTYPE html>\n'
-                     '<html lang="es">\n'
-                     '<head>\n'
-                     '    <meta charset="UTF-8">\n'
-                     '</head>\n'
-                     '<body>\n'
-                     '<p>Empresas</p>'
-                     '\n'
-                     '<ul>\n')
-
+       '<html lang="es">\n'
+       '<head>\n'
+       '    <meta charset="UTF-8">\n'
+       '</head>\n'
+       '<body>\n'
+       '<p>Empresas</p>'
+       '\n'
+       '<ul>\n')
+        
         for i in range(len(drug)):
-            id = drug[i]["id"]
-            manufacturer = drug[i]["openfda"]["manufacturer_name"][0]
-
-            contenido += '<p>' + id + manufacturer + '</p>'
-
+            id= drug[i]["id"]
+            manufacturer=drug[i]["openfda"]["manufacturer_name"][0]
+                
+            contenido+= '<p>'+id+manufacturer+'</p>'
+        
         return contenido
-
+    
+    
+    
     def do_GET(self):
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        path = self.path
-
+        
+        path=self.path
+        
         if '&' in path:
-            path = path.replace('&', '?')
-
+            path=path.replace('&', '?')
+        
         if '%3C' in path:
-            path = path.replace("%3C", "").replace("%3E", "")
+            path=path.replace("%3C", "").replace("%3E", "")
             print(self.path)
-
+        
+        
         if '?' in path:
 
-            parametros = path.split("?")[1:]
-
-            solicitud = path.split("?")[0]
-            print("Solicitud", solicitud)
-
-            # if "&" in parametros:
-            #   parametros=parametros.split
-
+            parametros=path.split("?")[1:]
+        
+            solicitud=path.split("?")[0]
+            print("Solicitud:", solicitud)
+                   
             for i in range(len(parametros)):
-                parameters = parametros[i].split("=")
-
-                if parameters[0] == 'limit':
-                    limit = parameters[1]
+                parameters=parametros[i].split("=")
+                
+                
+                if parameters[0]=='limit':
+                    limit=parameters[1]
                     print("Límite:", limit)
-
+                    
                 else:
-                    limit = 1
-
-                if parameters[0] == "company":
-                    name = parameters[1]
-                    busqueda = "manufacturer_name"
+                    limit=1
+                
+                if parameters[0]=="company":
+                    name=parameters[1]
+                    busqueda= "manufacturer_name"
                     print("Nombre:", name)
                     print("Búsqueda:", busqueda)
-
-                if parameters[0] == 'active_ingredient':
-                    name = parameters[1]
-                    busqueda = parameters[0]
+                
+                    
+                if parameters[0]=='active_ingredient':
+                    name=parameters[1]
+                    busqueda=parameters[0]
                     print("Nombre:", name)
                     print("Búsqueda:", busqueda)
-
+                                
         else:
-            limit = 1
-
+            limit=1
+            solicitud=""
+            
         print('Número de recursos solicitados:', limit)
-
-        if self.path == "/" or self.path == "":
-            mensaje = ''' 
+        
+        
+        if self.path=="/" or self.path=="":
+            mensaje= ''' 
                 <p><h2>Listado de medicamentos:</h2></p>
                 <p><form action="/listDrugs">
                       Limite:<input type="text" name='limit' value="1">
                 <p><input type="submit" value="Aceptar"></p>
                 </form> 
-
+                
                 <p><h2>Listado de empresas:</h2></p>
                 <form action = "/listCompanies" method="get">
                       Limite: <input type="text" name="limit" value="1">
                 <input type="submit" value="Aceptar">
-
+                    
                 </form>
-
+                
                 <p><h2>Busqueda de farmacos:</h2></p>
                 <form action = "/searchDrug" method="get">
                       Principio activo: <input type="text" name="active_ingredient" value="acetylsalicylic">
                       Limite: <input type="text" name="limit" value="1">
-
+                      
                 <input type="submit" value="Aceptar">
-
+                    
                 </form>
-
+                
                 <p><h2>Busqueda de empresas:</h2></p>
                 <form action = "/searchCompany" method="get">
                       Empresa: <input type="text" name="company" value="carefusion">
                       Limite: <input type="text" name="limit" value="1">
-
+                      
                 <input type="submit" value="Aceptar">
-
+                    
                 </form>
 
 
                 '''
-
-        elif self.path == '/listDrugs' or self.path == '/listDrugs?limit={}'.format(limit):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(mensaje, "utf8"))
+        
+        elif path== '/listDrugs' or path== '/listDrugs?limit={}'.format(limit):
             mensaje = self.get_listDrugs(limit)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(mensaje, "utf8"))
+        
+        elif path=='/listCompanies' or path== '/listCompanies?limit={}'.format(limit):
+            mensaje= self.get_listCompanies(limit)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(mensaje, "utf8"))
+            
+        elif path=='/listWarnings' or path=='/listWarnings?limit={}'.format(limit):
+            mensaje=self.get_listWarnings(limit)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(mensaje, "utf8"))
+            
+        elif solicitud=="/searchDrug":
+            mensaje=self.get_searchDrug(limit, busqueda, name)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(mensaje, "utf8"))
+        
+        elif solicitud=="/searchCompany":
+            mensaje=self.get_searchCompany(limit, busqueda, name)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(mensaje, "utf8"))
+        
+        elif path=='/secret':
+            self.send_error(401)
+            self.send_header('WWW-Authenticate', 'Basic realm="Mi servidor"')
+            self.end_headers()
+            
+        elif  path=='/redirect':
+            self.send_response(302)
+            self.send_header('Location', 'http://localhost:'+str(PORT))
+            self.end_headers()
+            
+        else:
 
-        elif self.path == '/listCompanies' or self.path == '/listCompanies?limit={}'.format(limit):
-            mensaje = self.get_listCompanies(limit)
-
-        elif solicitud == "/searchDrug":
-            mensaje = self.get_searchDrug(limit, busqueda, name)
-
-        elif solicitud == "/searchCompany":
-            mensaje = self.get_searchCompany(limit, busqueda, name)
-
-        self.wfile.write(bytes(mensaje, "utf8"))
+            self.send_error(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("I don't know '{}'.".format(path).encode())     
+ 
         print('Lista enviada')
+        
         return
-
-
+    
+    
 Handler = TestHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
+print("Sirviendo en el puerto", PORT)
 
 try:
     httpd.serve_forever()
